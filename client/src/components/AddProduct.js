4cd3b211cce2c4531949df07da3814bf1ac7ed76
batuhan öztürk <batuhandoruk91@gmail.com>
2021-03-29 20:58:24 +0300

    add dropdown feature

--- client/src/components/AddProduct.js
+++ client/src/components/AddProduct.js
@@ -83,60 +83,67 @@ function AddProduct(props) {
   }
 
   return (
-    <form className="p-3 bg-light" onSubmit={handleSubmit}>
-      <h5 className="mb-3 text-center">Add Product</h5>
 
-      <div className="row mb-3">
-        <div className="col-sm-4">
-          <div className="form-floating">
-            <input type="text" className="form-control" id="inputName" name="name" value={product.name} onChange={handleInputChange} placeholder="Name" />
-            <label htmlFor="inputName">Name</label>
+    <div class="btn-group dropup w-100">
+      <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+        Add Product
+      </button>
+      <div class="dropdown-menu w-100">
+        <form className="p-3 bg-light" onSubmit={handleSubmit}>
+
+          <div className="row mb-3">
+            <div className="col-sm-4">
+              <div className="form-floating">
+                <input type="text" className="form-control" id="inputName" name="name" value={product.name} onChange={handleInputChange} placeholder="Name" />
+                <label htmlFor="inputName">Name</label>
+              </div>
+            </div>
+
+            <div className="col-sm-4">
+              <div className="form-floating">
+                <input type="text" className="form-control" id="inputPrice" name="price" value={product.price} onChange={handleInputChange} placeholder="Price "/>
+                <label htmlFor="inputPrice">Price</label>
+              </div>
+            </div>
+
+            <div className="col-sm-4 p-0">
+                <input type="checkbox" className="btn-check" id="btnXS" name="size" value="XS" onClick={handleInputChange} autoComplete="off" />
+                <label className="btn btn-outline-primary m-1 p-1" htmlFor="btnXS">XS</label>
+
+                <input type="checkbox" className="btn-check" id="btnS" name="size" value="S" onClick={handleInputChange} autoComplete="off" />
+                <label className="btn btn-outline-primary m-1 p-1" htmlFor="btnS">S</label>
+
+                <input type="checkbox" className="btn-check" id="btnM" name="size" value="M" onClick={handleInputChange} autoComplete="off" />
+                <label className="btn btn-outline-primary m-1 p-1" htmlFor="btnM">M</label>
+
+                <input type="checkbox" className="btn-check" id="btnL" name="size" value="L" onClick={handleInputChange} autoComplete="off" />
+                <label className="btn btn-outline-primary m-1 p-1" htmlFor="btnL">L</label>
+            </div>
           </div>
-        </div>
 
-        <div className="col-sm-4">
-          <div className="form-floating">
-            <input type="text" className="form-control" id="inputPrice" name="price" value={product.price} onChange={handleInputChange} placeholder="Price "/>
-            <label htmlFor="inputPrice">Price</label>
+          <div className="form-floating mb-3">
+            <input type="text" className="form-control" id="inputDescription" name="description" value={product.description} onChange={handleInputChange} placeholder="Description" />
+            <label htmlFor="inputDescription">Description</label>
           </div>
-        </div>
-
-        <div className="col-sm-4 p-0">
-            <input type="checkbox" className="btn-check" id="btnXS" name="size" value="XS" onClick={handleInputChange} autoComplete="off" />
-            <label className="btn btn-outline-primary m-1 p-1" htmlFor="btnXS">XS</label>
-
-            <input type="checkbox" className="btn-check" id="btnS" name="size" value="S" onClick={handleInputChange} autoComplete="off" />
-            <label className="btn btn-outline-primary m-1 p-1" htmlFor="btnS">S</label>
-
-            <input type="checkbox" className="btn-check" id="btnM" name="size" value="M" onClick={handleInputChange} autoComplete="off" />
-            <label className="btn btn-outline-primary m-1 p-1" htmlFor="btnM">M</label>
-
-            <input type="checkbox" className="btn-check" id="btnL" name="size" value="L" onClick={handleInputChange} autoComplete="off" />
-            <label className="btn btn-outline-primary m-1 p-1" htmlFor="btnL">L</label>
-        </div>
+          <div className="row">
+            <div className="col-sm-5">
+            <select className="form-select" id="inputClass" aria-label="Select Audience" value={audience} onChange={handleAudienceChange} required>
+              <option value="">Select Audience</option>
+              <option value="human">Human</option>
+              <option value="pet">Pet</option>
+            </select>
+            </div>
+            <div className="col-sm-5">
+              <select className="form-select" id="inputCatType" name="categoryId" value={product.categoryId} aria-label="Select Type" onChange={handleInputChange} required>
+                <option value="">Select Type</option>
+                <GetCategories audience={audience}/>
+              </select>
+            </div>
+            <button type="submit" className="btn btn-primary col-sm-2">Submit</button>
+          </div>
+        </form>
       </div>
-
-      <div className="form-floating mb-3">
-        <input type="text" className="form-control" id="inputDescription" name="description" value={product.description} onChange={handleInputChange} placeholder="Description" />
-        <label htmlFor="inputDescription">Description</label>
-      </div>
-      <div className="row">
-        <div className="col-sm-5">
-        <select className="form-select" id="inputClass" aria-label="Select Audience" value={audience} onChange={handleAudienceChange} required>
-          <option value="">Select Audience</option>
-          <option value="human">Human</option>
-          <option value="pet">Pet</option>
-        </select>
-        </div>
-        <div className="col-sm-5">
-          <select className="form-select" id="inputCatType" name="categoryId" value={product.categoryId} aria-label="Select Type" onChange={handleInputChange} required>
-            <option value="">Select Type</option>
-            <GetCategories audience={audience}/>
-          </select>
-        </div>
-        <button type="submit" className="btn btn-primary col-sm-2">Submit</button>
-      </div>
-    </form>
+    </div>
 
 
   )
