8a64eb86dd43d969f44a52d9af33e0fe39b91c95
batuhan öztürk <batuhandoruk91@gmail.com>
2021-04-01 21:05:28 +0300

    add order track model

---
+++ models/order-model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const mongoOrderSchema = new mongoose.Schema({
+  productName: String,
+  productSize: String,
+  productPrize: String,
+  createdAt: String,
+  deliveryMethod: String,
+  deliveryAddress: String,
+  userId: String
+});
+
+module.exports = new mongoose.model('User', mongoOrderSchema);
