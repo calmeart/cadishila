432f46b75bf064262abd0a97ab7b6140f37ce7a1
batuhan öztürk <batuhandoruk91@gmail.com>
2021-04-01 21:09:51 +0300

    add review model

---
+++ models/review-model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const mongoReviewSchema = new mongoose.Schema({
+  userId: String,
+  productId: String,
+  reviewBody: String,
+  score: String,
+  images: [String],
+  createdAt: String
+});
+
+module.exports = new mongoose.model('Review', mongoReviewSchema);
