c0366a3e76187e002b5e6089632d4d8390b64e78
batuhan öztürk <batuhandoruk91@gmail.com>
2021-04-03 19:06:48 +0300

    fix jsx issues

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -28,10 +28,10 @@ function App() {
 
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
               <div className="container">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                  <span class="navbar-toggler-icon"></span>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
                   <ul className="navbar-nav">
                     <li className="nav-item">
                       <Link className={activeLink === "home" ? "nav-link active" : "nav-link"} to="/" name="home" onClick={handleNavbarClick} >Home</Link>
