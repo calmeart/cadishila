0b795fdf774adcdb3db8221e6d430e779df445c6
batuhan öztürk <batuhandoruk91@gmail.com>
2021-04-14 13:22:29 +0300

    define states and handle change functions

--- client/src/pages/payment.js
+++ client/src/pages/payment.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 
 import ProductCard from "../components/ProductCard";
+import { AuthContext } from "../context/auth";
 
 function Payment({ cartContent }) {
 
@@ -14,6 +15,38 @@ function Payment({ cartContent }) {
       totalPrice = cartContent.reduce(reducer, 0);
     }
 
+    const { user } = useContext(AuthContext);
+
+    const [userDetails, setUserDetails] = useState({
+      username: user ? user.username : "",
+      email: user ? user.email : "",
+      phone: ""
+    })
+
+    const [deliveryDetails, setDeliveryDetails] = useState({
+      city: "",
+      neighborhood: "",
+      addressOne: "",
+      addressTwo: "",
+      zipCode: ""
+    });
+
+    function handleUserChange(e) {
+      const { name, value } = e.target;
+      setUserDetails(prev => ({
+        ...prev,
+        [name]: value
+      }))
+    };
+
+    function handleDeliveryChange(e) {
+      const { name, value } = e.target;
+      setDeliveryDetails(prev => ({
+        ...prev,
+        [name]: value
+      }))
+    }
+
     return (
       <div id="payment">
         <div className="row">
@@ -21,16 +54,32 @@ function Payment({ cartContent }) {
             <div className="container">
               <h2 className="text-center">Contact Info</h2>
               <h5>Personal Information</h5>
-              <input type="text" className="form-control mb-3" name="fname" placeholder="First Name" />
-              <input type="text" className="form-control mb-3" name="lname" placeholder="Last Name" />
-              <input type="text" className="form-control mb-3" name="phone" placeholder="Phone Number" />
-              <input type="email" className="form-control mb-3" name="email" placeholder="Contact Mail" />
+              <div className="row">
+                <div className="col-md-12">
+                  <input type="text" className="form-control mb-3" name="username" value={userDetails.username} onChange={handleUserChange} placeholder="Name Surname" />
+                </div>
+                <div className="col-md-6">
+                  <input type="email" className="form-control mb-3" name="email" value={userDetails.email} onChange={handleUserChange} placeholder="Contact Mail" />
+                </div>
+                <div className="col-md-6">
+                  <input type="text" className="form-control mb-3" name="phone" value={userDetails.phone} onChange={handleUserChange} placeholder="Phone Number" />
+                </div>
 
+              </div>
               <h5>Delivery Details</h5>
-              <input type="text" className="form-control mb-3" name="city" placeholder="City" />
-              <input type="text" className="form-control mb-3" name="neighborhood" placeholder="Neighborhood" />
-              <input type="text" className="form-control mb-3" name="addressLineOne" placeholder="Address Line 1" />
-              <input type="text" className="form-control mb-3" name="addressLineTwo" placeholder="Address Line 2" />
+              <div className="row">
+                <div className="col-md-6">
+                  <input type="text" className="form-control mb-3" name="city" value={deliveryDetails.city} onChange={handleDeliveryChange} placeholder="City" />
+                </div>
+                <div className="col-md-6">
+                  <input type="text" className="form-control mb-3" name="neighborhood" value={deliveryDetails.neighborhood} onChange={handleDeliveryChange} placeholder="Neighborhood" />
+                </div>
+                <div className="col-md-12">
+                  <input type="text" className="form-control mb-3" name="addressOne" value={deliveryDetails.addressOne} onChange={handleDeliveryChange} placeholder="Address Line 1" />
+                  <input type="text" className="form-control mb-3" name="addressTwo" value={deliveryDetails.addressTwo} onChange={handleDeliveryChange} placeholder="Address Line 2" />
+                  <input type="text" className="form-control mb-3" name="zipCode" value={deliveryDetails.zipCode} onChange={handleDeliveryChange} placeholder="Zip Code" />
+                </div>
+              </div>
             </div>
           </div>
           <div className="col-md-6 text-center">
