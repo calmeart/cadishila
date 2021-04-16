f32d7fcec15708b9bb34fe473bff2720adf5273a
batuhan öztürk <batuhandoruk91@gmail.com>
2021-04-16 21:44:39 +0300

    change navbar style

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -10,39 +10,18 @@ function NavBar() {
   const path = pathname === "/" ? "home" : pathname.substr(1);
   const [activeLink, setActiveLink] = useState(path);
 
+  const { innerWidth } = window;
+  const [isMobile] = useState( (innerWidth < 576) ? true : false );
+
   function handleNavbarClick(e) {
     setActiveLink(e.target.name);
+    const navbarList = document.getElementById("navbarSupportedContent");
+    navbarList.className = "collapse navbar-collapse";
   };
 
   return (
-    context.user ? (
-      <nav className="navbar navbar-expand-lg navbar-dark">
-        <div className="container">
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <Link className={activeLink === "home" ? "nav-link active" : "nav-link"} to="/" name="home" onClick={handleNavbarClick} >Home</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link active" to={'/users/' + context.user.id} name={context.user.username} >{context.user.username}</Link>
-              </li>
-              <li className="nav-item">
-                <a id="cartButton" className="nav-link" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
-                  Cart
-                </a>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/login" name="logout" onClick={context.logout} >Logout</Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    ) : (
-      <nav className="navbar navbar-expand-lg navbar-dark">
+
+      <nav className="navbar fixed-top navbar-expand-sm navbar-dark">
         <div className="container">
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
@@ -56,18 +35,43 @@ function NavBar() {
                 <Link className={activeLink === "about" ? "nav-link active" : "nav-link"} to="/about" name="about" onClick={handleNavbarClick} >About</Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+                <Link className={activeLink === "products" ? "nav-link active" : "nav-link"} to="/products" name="products" onClick={handleNavbarClick} >Products</Link>
+              </li>
+              {!isMobile &&
+              <li className="nav-item">
+                <a id="cartButton" className="nav-link" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
                   Cart
                 </a>
-              </li>
-              <li className="nav-item">
-                <Link className={activeLink === "login" ? "nav-link active" : "nav-link"} to="/login" name="login" onClick={handleNavbarClick} >Login</Link>
-              </li>
+              </li> }
+              {context.user ? (
+                <li className="nav-item dropdown">
+                  <a className="nav-link dropdown-toggle" href="/dummy" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    {context.user.username}
+                  </a>
+                  <ul className="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDropdown">
+                    <li>
+                      <Link className="dropdown-item" to={'/users/' + context.user.id} name={context.user.username} >View Profile</Link>
+                    </li>
+                    <li><hr className="dropdown-divider" /></li>
+                    <li>
+                      <Link className="dropdown-item" to="/login" name="logout" onClick={context.logout} >Logout</Link>
+                    </li>
+                  </ul>
+
+                </li>
+              ) : (
+                <li className="nav-item">
+                  <Link className={activeLink === "login" ? "nav-link active" : "nav-link"} to="/login" name="login" onClick={handleNavbarClick} >Login</Link>
+                </li>
+              )}
             </ul>
           </div>
+          {isMobile && <a id="cartButton" className="nav-item nav-link" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+            Cart
+          </a>}
         </div>
       </nav>
-    )
+
   )
 }
 
