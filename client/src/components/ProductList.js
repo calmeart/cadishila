b9d6534ce91e9599afe9376f794e5a970298c552
batuhan öztürk <batuhandoruk91@gmail.com>
2021-04-17 18:17:50 +0300

    add spinners to product List

--- client/src/components/ProductList.js
+++ client/src/components/ProductList.js
@@ -4,21 +4,18 @@ import { useQuery } from '@apollo/client';
 import { GetProductsQuery } from '../graphql/queries';
 import { GetCategoriesQuery } from '../graphql/category-queries';
 import ProductCard from "./ProductCard";
+import CenteredSpinner from "../utils/CenteredSpinner";
 
 function DisplayProducts({ audience, propArray }) {
   const { loading, error, data } = useQuery(GetProductsQuery);
 
-  if (loading) return <p className="errorMessage">Loading...</p>;
+  if (loading) return <CenteredSpinner />;
   if (error) return <p className="errorMessage">Error :(</p>;
 
   return (
-    <div id="products" className="container">
-      {
       data.products.filter(item => item.category.audience === audience)
       .filter(item => propArray.indexOf(item.category.name) > -1 )
       .map(item => <ProductCard key={item.id} item={item} /> )
-      }
-    </div>
   );
 }
 
@@ -31,10 +28,8 @@ function VerticalCheckButton({ item, onCheckBoxChange }) {
   )
 };
 
+function DisplayCategories({ audience, categoryFilter, setCategoryFilter }) {
 
-function ProductList({ audience }) {
-
-  const [categoryFilter, setCategoryFilter] = useState([]);
   const [selectAll, setSelectAll] = useState(true);
 
   const { loading, error } = useQuery(GetCategoriesQuery, {onCompleted: data => {
@@ -44,7 +39,7 @@ function ProductList({ audience }) {
     setCategoryFilter(categoriesArray);
   }});
 
-  if (loading) return <p className="errorMessage">Loading...</p>;
+  if (loading) return <CenteredSpinner />;
   if (error) return <p className="errorMessage">Error :(</p>;
 
   function onCheckBoxChange(e) {
@@ -61,6 +56,26 @@ function ProductList({ audience }) {
     setCategoryFilter(tempArray);
   }
 
+  return (
+    <>
+      <input type="checkbox" className="btn-check" name="checkAll" id="btncheckAll" autoComplete="off" onChange={onCheckBoxChange} checked={selectAll} />
+      <label className="btn btn-outline-secondary w-100" htmlFor="btncheckAll">Select All</label>
+      <hr className="dropdown-divider" />
+      <div className="btn-group-vertical w-100" role="group" aria-label="Vertical category button group">
+        {
+          categoryFilter.map(item => <VerticalCheckButton key={item.id} item={item} onCheckBoxChange={onCheckBoxChange} />)
+        }
+      </div>
+    </>
+  )
+}
+
+
+function ProductList({ audience }) {
+
+  const [categoryFilter, setCategoryFilter] = useState([]);
+
+
   const propArray = categoryFilter.filter(item => item.isChecked === true).map(item => item.name);
 
   return (
@@ -69,16 +84,11 @@ function ProductList({ audience }) {
       <div className="buttonGroup text-center">
         <h5>Filter Categories</h5>
         <hr className="dropdown-divider" />
-        <input type="checkbox" className="btn-check w-100" name="checkAll" id="btncheckAll" autoComplete="off" onChange={onCheckBoxChange} checked={selectAll}/>
-        <label className="btn btn-outline-secondary w-100" htmlFor="btncheckAll">Select All</label>
-        <hr className="dropdown-divider" />
-        <div className="btn-group-vertical w-100" role="group" aria-label="Vertical category button group">
-          {
-            categoryFilter.map(item => <VerticalCheckButton key={item.id} item={item} onCheckBoxChange={onCheckBoxChange} />)
-          }
-        </div>
+        <DisplayCategories audience={audience} categoryFilter={categoryFilter} setCategoryFilter={setCategoryFilter} />
       </div>
-      <DisplayProducts audience={audience} propArray={propArray}/>
+      <div id="products">
+        <DisplayProducts audience={audience} propArray={propArray}/>
+      </div>
     </div>
 
   )
